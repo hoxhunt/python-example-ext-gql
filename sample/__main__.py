a293a10b8e47f57a1dbfb860ae95fd4e45576c58
Pyry Åvist <pyry.avist@hoxhunt.com>
2020-09-24 19:45:24 +0300

    fix: token from env and prod domain

--- sample/__main__.py
+++ sample/__main__.py
@@ -1,8 +1,9 @@
 from .api import HoxhuntApi
 from .api import IncidentsIter
+import os
 
-AUTH_TOKEN = ""
-API_URL = "https://app.hoxhunt.dev/graphql-external"
+AUTH_TOKEN = os.getenv('HOXHUNT_AUTH_TOKEN')
+API_URL = "https://app.hoxhunt.com/graphql-external"
 
 if __name__ == '__main__':
     api = HoxhuntApi(auth_token=AUTH_TOKEN, api_url=API_URL)
